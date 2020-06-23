eed330fb2cd1f3dda5b6c082ea2ae1df5b59ad25
Mike Vong <mv910@hotmail.com>
2020-06-23 16:51:34 -0700

    fix loading issue

--- server.js
+++ server.js
@@ -7,11 +7,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
 
 // Define API routes here
 
